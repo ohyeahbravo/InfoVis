9e20e753ebab6e26a0ebc90db3e38b7deca65ae3
Hiyeon <ohyeahbravo@gmail.com>
2018-06-19 11:31:04 +0200

    ass4 : fisheye works?

--- src/infovis/diagram/layout/Fisheye.java
+++ src/infovis/diagram/layout/Fisheye.java
@@ -16,30 +16,25 @@ import java.util.Iterator;
 
 public class Fisheye implements Layout{
 
-	int d = 2;	// distortion factor
-	
-	int focusX;
-	int focusY;
-	
+	int d = 5;	// distortion factor
+		
 	int PfishX;
 	int PfishY;
 	
-	double DmaxX;
-	double DmaxY;
 	double DnormX;
 	double DnormY;
 	
 	public void setMouseCoords(int x, int y, View view) {
 		// TODO Auto-generated method stub
 		
-		focusX = view.getMousePosition().x;
-		focusY = view.getMousePosition().y;
+		double focusX = view.getMousePosition().x;
+		double focusY = view.getMousePosition().y;
 
 		DnormX = x - focusX;
 		DnormY = y - focusY;
 		
-		DmaxX = (DnormX >= 0 ? view.getWidth() - focusX : 0 - focusX);
-		DmaxY = (DnormY >= 0 ? view.getHeight() - focusY : 0 - focusY);
+		double DmaxX = (DnormX >= 0 ? view.getWidth() - focusX : 0 - focusX);
+		double DmaxY = (DnormY >= 0 ? view.getHeight() - focusY : 0 - focusY);
 		
 		PfishX = (int) (focusX + G(DnormX/DmaxX)*DmaxX);
 		PfishY = (int) (focusY + G(DnormY/DmaxY)*DmaxY);
@@ -50,9 +45,43 @@ public class Fisheye implements Layout{
 		
 		Model fishModel = new Model();
 		
+		double ratio;
+		double PnormX, PnormY;
+		double SnormX, SnormY;
+		double QnormX, QnormY;
+		double SgeomX, SgeomY;
+		
 		for (Vertex vertex: model.getVertices()){
-			setMouseCoords((int) vertex.getX(), (int) vertex.getY(), view);
-			fishModel.addVertex(new Vertex(PfishX, PfishY));
+			
+			// center of the vertex
+			PnormX = vertex.getCenterX();
+			PnormY = vertex.getCenterY();
+
+			// width & height of a vertex bounding box
+			SnormX = vertex.getWidth();
+			SnormY = vertex.getHeight();
+			ratio = SnormX / SnormY;
+			
+			setMouseCoords((int) PnormX, (int) PnormY, view);
+			Vertex fishVertex = new Vertex(PfishX + SnormX/2, PfishY + SnormY/2);
+			
+			// point on the box in the direction of the focus point
+			QnormX = (DnormX >= 0 ? PnormX + SnormX/2 : PnormX - SnormX/2);
+			QnormY = (DnormY >= 0 ? PnormY + SnormY/2 : PnormY - SnormY/2);
+			
+			// new geometric size
+			setMouseCoords((int) QnormX, (int) QnormY, view);
+			SgeomX = 2 * Math.abs(PfishX - fishVertex.getCenterX());
+			SgeomY = 2 * Math.abs(PfishY - fishVertex.getCenterY());
+			
+			if(SgeomX < SgeomY)
+				SgeomY = SgeomX / ratio;
+			else SgeomX = SgeomY * ratio;
+
+			fishVertex.setWidth(SgeomX);
+			fishVertex.setHeight(SgeomY);
+			
+			fishModel.addVertex(fishVertex);
 		}
 		
 		return fishModel;
